02145f371bb5653482529a4974c00731297c7794
Biancaa Ramesh <87770461+Biancaa-R@users.noreply.github.com>
2025-10-26 00:55:52 +0530

    support for ITM initialization and character sending
    Refactor ITM functions and add documentation.

--- All_source_codes/target/itm_send_data.c
+++ All_source_codes/target/itm_send_data.c
@@ -1,31 +1,36 @@
+/* Defines for ITM */
+#define DEMCR_TRCENA         (1U << 24)
+#define DEMCR                *((volatile uint32_t*) 0xE000EDFCU)
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-//					Implementation of printf like feature using ARM Cortex M3/M4/ ITM functionality
-//					This function will not work for ARM Cortex M0/M0+
-//					If you are using Cortex M0, then you can use semihosting feature of openOCD
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define ITM_STIMULUS_PORT0   *((volatile uint32_t*) 0xE0000000)
+#define ITM_TRACE_ENABLE     *((volatile uint32_t*) 0xE0000E00)
+#define ITM_TRACE_EN_PORT0   (1U << 0)
 
 
-//Debug Exception and Monitor Control Register base address
-#define DEMCR        			*((volatile uint32_t*) 0xE000EDFCU )
+/**
+  * @brief  Initializes the ITM hardware for tracing.
+  * @brief  This function should be called ONCE from your main() function.
+  * @param  None
+  * @retval None
+  */
+void ITM_Init(void)
+{
+    DEMCR |= DEMCR_TRCENA;          // Enable TRCENA
+    ITM_TRACE_ENABLE |= ITM_TRACE_EN_PORT0; // Enable ITM stimulus port 0
+}
 
-/* ITM register addresses */
-#define ITM_STIMULUS_PORT0   	*((volatile uint32_t*) 0xE0000000 )
-#define ITM_TRACE_EN          	*((volatile uint32_t*) 0xE0000E00 )
-
+/**
+  * @brief  Transmits a single character over ITM stimulus port 0.
+  * @param  ch: The character to transmit
+  * @retval None
+  */
 void ITM_SendChar(uint8_t ch)
 {
+    // Wait until ITM TxFIFO is ready (bit 0 is 1)
+    while (!(ITM_STIMULUS_PORT0 & 1));
 
-	//Enable TRCENA
-	DEMCR |= ( 1 << 24);
-
-	//enable stimulus port 0
-	ITM_TRACE_EN |= ( 1 << 0);
-
-	// read FIFO status in bit [0]:
-	while(!(ITM_STIMULUS_PORT0 & 1));
-
-	//Write to ITM stimulus port0
-	ITM_STIMULUS_PORT0 = ch;
+    // Write to ITM stimulus port 0
+    // Note: The port is 32-bit, but we only write a byte.
+    ITM_STIMULUS_PORT0 = (uint32_t)ch;
 }
 
